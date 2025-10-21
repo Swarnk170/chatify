c8ac6104acfdecc8b83f876795e035e2d0bd4f7b
Swarn <swarnchaudhary170@gmail.com>
2025-10-21 10:08:33 +0545

    error and bugs fixed

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -6,10 +6,11 @@ import {
   sendMessage,
 } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = Router();
 
-router.use(protectRoute);
+router.use(arcjetProtection, protectRoute);
 router.get(("/contacts", getAllContacts));
 router.get("/chat", getChatPartners);
 router.get("/:id", getMessagesByUserId);
