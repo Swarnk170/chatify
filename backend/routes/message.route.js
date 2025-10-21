f9c0762861d19ff8c6bb3032d8152130fa5f10da
Swarn <swarnchaudhary170@gmail.com>
2025-10-21 10:04:39 +0545

    messages functionality added

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
+
+router.use(protectRoute);
+router.get(("/contacts", getAllContacts));
+router.get("/chat", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
